14868c4ac76504fe70b7b096cf3cfa6ed6e5410a
darwinagyei <agyeidarwin@gmail.com>
2023-09-30 20:16:48 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-/**
- * Triggered from a message using Google Cloud Pub/Sub topic 
- *
- * @param {!Object} event Event payload.
- * @param {!Object} context Metadata for the event.
- */
+import { newAccessToken } from "./utils/APIs/spotify/new-access-token.js";
+import { searchQuery } from './utils/APIs/spotify/search-query.js';
+import { tweetSearchItem } from './utils/APIs/twitter/tweet-search-item.js';
 
-import { tweetSearchItem } from './twitter-api-requests/tweet-search-item.js';
-export async function helloPubSub(event, context) {
-    const tweetSearchQuery = await tweetSearchItem();
-    console.log(tweetSearchQuery);
 
+export function helloPubSub(event,context) {
+
+    newAccessToken().then((response1) => {
+        const access_token = response1.data.access_token;
+        return searchQuery("afrobeat", "track", access_token);
+    }).then((response2) => {
+
+        const json_data = response2.data;
+        const external_urls = json_data["tracks"]["items"][0].external_urls.spotify
+        return tweetSearchItem(external_urls)
+    }).then((response3) => {
+        const tweet_spotify_url = response3.data.data;
+        console.log(tweet_spotify_url)
+    }).catch((error) => {
+        console.error('Error:', error);
+
+    })
 }
+
+helloPubSub();
