e949ce6efa3e0e2280c1d50dcb283b5ea80d223f
darwinagyei <agyeidarwin@gmail.com>
2023-10-02 14:44:48 -0400

    modified files

--- index.js
+++ index.js
@@ -3,23 +3,21 @@ import { searchQuery } from './utils/APIs/spotify/search-query.js';
 import { tweetSearchItem } from './utils/APIs/twitter/tweet-search-item.js';
 
 
-export function helloPubSub(event,context) {
 
-    newAccessToken().then((response1) => {
-        const access_token = response1.data.access_token;
-        return searchQuery("afrobeat", "track", access_token);
-    }).then((response2) => {
 
-        const json_data = response2.data;
-        const external_urls = json_data["tracks"]["items"][0].external_urls.spotify
-        return tweetSearchItem(external_urls)
-    }).then((response3) => {
-        const tweet_spotify_url = response3.data.data;
-        console.log(tweet_spotify_url)
-    }).catch((error) => {
-        console.error('Error:', error);
+export const handler = async (event) => {
 
-    })
-}
+    try {
 
-helloPubSub();
+        const spotify_access_token = await newAccessToken();
+        const access_token = spotify_access_token.access_token;
+        const spotifySearchItem = await searchQuery("afrobeat", "track", access_token);
+        const external_urls = spotifySearchItem["tracks"]["items"][0].external_urls.spotify;
+        const tweet_search_item = await tweetSearchItem(external_urls);
+        return(tweet_search_item);
+
+    }catch(err){
+        console.error(err);
+    }
+
+};
